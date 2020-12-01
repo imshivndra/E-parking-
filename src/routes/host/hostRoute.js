039dfe478b5ad7c54d51157f5848455317861a2e
NISHIPATODI <nishi.patodi@gmail.com>
2020-12-02 00:29:06 +0530

    hostBookings

--- src/routes/host/hostRoute.js
+++ src/routes/host/hostRoute.js
@@ -8,6 +8,7 @@ const HostLocations=require("../../controllers/index").HostLocations;
 
 //router.post('/addparking',VerifyUserJWT,HostLocations.AddParking);
 router.post('/addparking',HostLocations.AddParking);
+router.get('/hostbooking',VerifyUserJWT,HostLocations.HostBookings);
 
 
 module.exports=router;
